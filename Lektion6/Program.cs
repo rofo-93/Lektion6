aa3871726a985ac3a4442f9ab45b4ffc405bb59f
rofo-93 <robin_forsberg93@hotmail.com>
2026-09-16 11:43:05 +0200

    Add project files.

---
+++ Lektion6/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Lektion6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Tryparse
+
+            //Console.WriteLine("Skriv ett heltal som du vill addera med 5.");
+            //string userInput = Console.ReadLine();
+            //int userNumber;
+
+            //while (!int.TryParse(userInput, out userNumber))
+            //{
+            //    Console.WriteLine("Du måste ange ett heltal, försök igen");
+            //    userInput = Console.ReadLine();
+            //}
+
+            //int totaloftwonumbers = 5 + userNumber;
+            //Console.WriteLine(totaloftwonumbers);
+
+
+
+            //Var och Dynamic
+
+
+            var age = 23 + " Robin " + 22.3;
+            var name = "Robin";
+            var isSuccess = true;
+            var decimalNumber = 22.3;
+
+            Console.WriteLine(age);
+
+
+            dynamic streetNumber = 3;
+            streetNumber = "Hej";
+            streetNumber = true;
+
+            Console.WriteLine(streetNumber);
+
+
+
+
+
+
+
+
+
+            var numbers = new int[3] { 7, 4, 2 };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
